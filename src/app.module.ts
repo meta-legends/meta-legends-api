3389ed0428c4b4c42df47cb62c4cfe8dbebddae9
handosensei <handosensei@gmail.com>
2023-05-02 12:41:31 +0200

    Docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,53 +1,45 @@
-import { MiddlewareConsumer, Module } from "@nestjs/common";
-import { DataSource } from "typeorm";
-import { ConfigModule } from "@nestjs/config";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-import { MintPackageController } from "./mint-package/mint-package.controller";
-import { MintPackageService } from "./mint-package/mint-package.service";
-import { MintPackageModule } from "./mint-package/mint-package.module";
-import { MintPackage } from "./mint-package/mint-package.entity";
-import { TokenRewardController } from "./token-reward/token-reward.controller";
-import { TokenRewardService } from "./token-reward/token-reward.service";
-import { TokenRewardModule } from "./token-reward/token-reward.module";
-import { AuthModule } from "./auth/auth.module";
-import { AuthMiddleware } from "./middleware/auth.middleware";
+import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { DataSource } from 'typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MintPackageController } from './mint-package/mint-package.controller';
+import { MintPackageService } from './mint-package/mint-package.service';
+import { MintPackageModule } from './mint-package/mint-package.module';
+import { MintPackage } from './mint-package/mint-package.entity';
+import { TokenRewardController } from './token-reward/token-reward.controller';
+import { TokenRewardService } from './token-reward/token-reward.service';
+import { TokenRewardModule } from './token-reward/token-reward.module';
+import { AuthModule } from './auth/auth.module';
+import { AuthMiddleware } from './middleware/auth.middleware';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: [".env.local", ".env"]
+      envFilePath: ['.env'],
     }),
     TypeOrmModule.forRoot({
-      type: "mysql",
+      type: 'mysql',
       host: process.env.DATABASE_HOST,
       port: 3306,
       username: process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_NAME,
       entities: [MintPackage],
-      synchronize: true
+      synchronize: true,
     }),
     MintPackageModule,
     TokenRewardModule,
-    AuthModule
+    AuthModule,
   ],
-  controllers: [
-    AppController,
-    MintPackageController,
-    TokenRewardController],
-  providers: [
-    AppService,
-    MintPackageService,
-    TokenRewardService
-  ]
+  controllers: [AppController, MintPackageController, TokenRewardController],
+  providers: [AppService, MintPackageService, TokenRewardService],
 })
 export class AppModule {
-  constructor(private dataSource: DataSource) {
-  }
+  constructor(private dataSource: DataSource) {}
 
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(AuthMiddleware).forRoutes("");
+    consumer.apply(AuthMiddleware).forRoutes('');
   }
 }
