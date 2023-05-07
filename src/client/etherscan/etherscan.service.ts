aa5e7aeecc00fae14692469112cde69cbc5d0bb5
handosensei <handosensei@gmail.com>
2023-05-07 16:52:30 +0200

    Method etherscan

--- src/client/etherscan/etherscan.service.ts
+++ src/client/etherscan/etherscan.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 
 @Injectable()
-export class EtherscanService {}
+export class EtherscanService {
+  async getNFTsByWallet(wallet: string) {
+    const mlContract = process.env.CONTRACT_ML;
+    const openSeaApiKey = process.env.ETHERSCAN_API_KEY;
+    const url = `https://api.etherscan.io/api?module=account&action=tokennfttx&contractaddress=${mlContract}&address=${wallet}&apikey=${openSeaApiKey}`;
+    try {
+      const response = await axios.get(url);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
