8c8f15fbba027b6639f5b84b3289d30c222877b0
handosensei <handosensei@gmail.com>
2023-05-27 19:16:10 +0200

    Refacto user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,16 @@
-import { Controller, Get, Header, Param } from '@nestjs/common';
+import { Controller, Get, Header, Param, Req, UseGuards } from '@nestjs/common';
 import { UserService } from '@src/user/user.service';
+import { AuthGuard } from '@src/auth/auth.guard';
+import { Request } from 'express';
 
 @Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
 
+  @UseGuards(AuthGuard)
   @Header('content-type', 'application/json')
-  @Get(':wallet/is-holder')
-  async isHolder(@Param('wallet') wallet: string) {
-    return this.userService.isHolder(wallet);
+  @Get('/is-holder')
+  async isHolder(@Req() request: Request) {
+    return this.userService.isHolder(request['user-wallet']);
   }
 }
