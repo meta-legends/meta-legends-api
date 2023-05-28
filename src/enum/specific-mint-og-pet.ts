edd836a79b618c1ad1233d2d59b65e11471377ee
handosensei <handosensei@gmail.com>
2023-05-28 09:09:56 +0200

    Constant Mint specific

--- src/enum/specific-mint-og-pet.ts
+++ src/enum/specific-mint-og-pet.ts
@@ -1,5 +1,3 @@
-import { Injectable } from '@nestjs/common';
-
 export const PREFERENCE_MINT = 'mint';
 export const PREFERENCE_OG = 'og';
 export const PREFERENCE_GUARDIAN = 'guardian';
@@ -7,6 +5,3 @@ export const PREFERENCE_JUDGE = 'judge';
 export const PREFERENCE_COUNCIL = 'council';
 export const PREFERENCE_WHALE = 'whale';
 export const PREFERENCE_HONORARY = 'honorary';
-
-@Injectable()
-export class PreferenceOgPetService {}
