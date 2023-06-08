a580620bbca4bde2849a56e761a7ada28624e6f0
handosensei <handosensei@gmail.com>
2023-06-08 09:33:49 +0200

    OG Pets const

---
+++ src/enum/og-pet-draw.ts
@@ -0,0 +1,219 @@
+export const REG_NAME_PET_ROUGH_TIERS_1 = 'CHICKENOX';
+export const REG_NAME_PET_ROUGH_TIERS_2 = 'ARACHIR';
+export const REG_NAME_PET_ROUGH_TIERS_3 = 'REKTOR';
+export const REG_NAME_PET_CYBER_TIERS_1 = 'SQUIRREX';
+export const REG_NAME_PET_CYBER_TIERS_2 = 'X-FANG';
+export const REG_NAME_PET_CYBER_TIERS_3 = 'C-HOUND';
+export const REG_NAME_PET_MATRIX_TIERS_1 = 'ARIOX';
+export const REG_NAME_PET_MATRIX_TIERS_2 = 'DOMENOX';
+export const REG_NAME_PET_MATRIX_TIERS_3 = 'XEPIROX';
+export const REG_NAME_PET_GOLDBOI_TIERS_1 = 'NISUS';
+export const REG_NAME_PET_GOLDBOI_TIERS_2 = 'ARRIUS';
+export const REG_NAME_PET_GOLDBOI_TIERS_3 = 'FAMUZA';
+export const REG_NAME_PET_ROBOTER_TIERS_1 = 'PRONGHAR';
+export const REG_NAME_PET_ROBOTER_TIERS_2 = 'RHINOZOR';
+export const REG_NAME_PET_ROBOTER_TIERS_3 = 'HREKTION';
+export const REG_NAME_PET_BURNER_TIERS_1 = 'ENFORCER';
+export const REG_NAME_PET_BURNER_TIERS_2 = 'DESTRUCTOR';
+export const REG_NAME_PET_BURNER_TIERS_3 = 'MANTIS';
+export const REG_NAME_PET_CELESTIAL_TIERS_1 = 'SPIRIG';
+export const REG_NAME_PET_CELESTIAL_TIERS_2 = 'DRAGNOR';
+export const REG_NAME_PET_CELESTIAL_TIERS_3 = 'FALCOZ';
+
+export const PERCENT_PET_ROUGH_TIERS_1 = 10;
+export const PERCENT_PET_ROUGH_TIERS_2 = 8;
+export const PERCENT_PET_ROUGH_TIERS_3 = 5;
+export const PERCENT_PET_CYBER_TIERS_1 = 9;
+export const PERCENT_PET_CYBER_TIERS_2 = 7;
+export const PERCENT_PET_CYBER_TIERS_3 = 4;
+export const PERCENT_PET_MATRIX_TIERS_1 = 8;
+export const PERCENT_PET_MATRIX_TIERS_2 = 6;
+export const PERCENT_PET_MATRIX_TIERS_3 = 3;
+export const PERCENT_PET_GOLDBOI_TIERS_1 = 7;
+export const PERCENT_PET_GOLDBOI_TIERS_2 = 4;
+export const PERCENT_PET_GOLDBOI_TIERS_3 = 2;
+export const PERCENT_PET_ROBOTER_TIERS_1 = 5;
+export const PERCENT_PET_ROBOTER_TIERS_2 = 4;
+export const PERCENT_PET_ROBOTER_TIERS_3 = 2;
+export const PERCENT_PET_BURNER_TIERS_1 = 5;
+export const PERCENT_PET_BURNER_TIERS_2 = 2.5;
+export const PERCENT_PET_BURNER_TIERS_3 = 1.5;
+export const PERCENT_PET_CELESTIAL_TIERS_1 = 4;
+export const PERCENT_PET_CELESTIAL_TIERS_2 = 2;
+export const PERCENT_PET_CELESTIAL_TIERS_3 = 1;
+
+export const SUPPLY_PET_ROUGH_TIERS_1 = 108;
+export const SUPPLY_PET_ROUGH_TIERS_2 = 86;
+export const SUPPLY_PET_ROUGH_TIERS_3 = 54;
+export const SUPPLY_PET_CYBER_TIERS_1 = 97;
+export const SUPPLY_PET_CYBER_TIERS_2 = 76;
+export const SUPPLY_PET_CYBER_TIERS_3 = 42;
+export const SUPPLY_PET_MATRIX_TIERS_1 = 86;
+export const SUPPLY_PET_MATRIX_TIERS_2 = 65;
+export const SUPPLY_PET_MATRIX_TIERS_3 = 32;
+export const SUPPLY_PET_GOLDBOI_TIERS_1 = 76;
+export const SUPPLY_PET_GOLDBOI_TIERS_2 = 42;
+export const SUPPLY_PET_GOLDBOI_TIERS_3 = 21;
+export const SUPPLY_PET_ROBOTER_TIERS_1 = 54;
+export const SUPPLY_PET_ROBOTER_TIERS_2 = 42;
+export const SUPPLY_PET_ROBOTER_TIERS_3 = 21;
+export const SUPPLY_PET_BURNER_TIERS_1 = 55;
+export const SUPPLY_PET_BURNER_TIERS_2 = 26;
+export const SUPPLY_PET_BURNER_TIERS_3 = 16;
+export const SUPPLY_PET_CELESTIAL_TIERS_1 = 42;
+export const SUPPLY_PET_CELESTIAL_TIERS_2 = 21;
+export const SUPPLY_PET_CELESTIAL_TIERS_3 = 10;
+
+export const SPECIFIC_PET_FOR_COUNCIL = 'council';
+export const SPECIFIC_PET_FOR_HONORARY = 'honorary';
+export const SPECIFIC_PET_FOR_GUARDIAN = 'guardian';
+export const SPECIFIC_PET_FOR_JUDGE = 'judge';
+export const SPECIFIC_PET_FOR_TOP10 = 'top 10';
+
+export const SUPPLY_PET_COUNCIL = 305;
+export const SUPPLY_PET_HONORARY = 66;
+export const SUPPLY_PET_GUARDIAN = 36;
+export const SUPPLY_PET_JUDGE = 11;
+export const SUPPLY_PET_TOP10 = 10;
+
+export const SPECIFIC_NAME_PET_GUARDIAN = 'Silver Roboter Pronghar';
+export const SPECIFIC_NAME_PET_COUNCIL = 'Council Matrix Xepirox';
+export const SPECIFIC_NAME_PET_TOP10 = 'Gold Celestial Falcoz';
+export const SPECIFIC_NAME_PET_JUDGE = 'Judge Goldboi Nisus';
+export const SPECIFIC_NAME_PET_HONORARY = 'Honorary Burner Mantis';
+
+export const OG_PET_SPECIFIC_SUPPLY = [
+  {
+    for: SPECIFIC_PET_FOR_COUNCIL,
+    pet: SPECIFIC_NAME_PET_GUARDIAN,
+    supply: SUPPLY_PET_COUNCIL,
+  },
+  {
+    for: SPECIFIC_PET_FOR_HONORARY,
+    pet: SPECIFIC_NAME_PET_COUNCIL,
+    supply: SUPPLY_PET_HONORARY,
+  },
+  {
+    for: SPECIFIC_PET_FOR_GUARDIAN,
+    pet: SPECIFIC_NAME_PET_TOP10,
+    supply: SUPPLY_PET_GUARDIAN,
+  },
+  {
+    for: SPECIFIC_PET_FOR_JUDGE,
+    pet: SPECIFIC_NAME_PET_JUDGE,
+    supply: SUPPLY_PET_JUDGE,
+  },
+  {
+    for: SPECIFIC_PET_FOR_TOP10,
+    pet: SPECIFIC_NAME_PET_HONORARY,
+    supply: SUPPLY_PET_TOP10,
+  },
+];
+
+export const OG_PET_REGULAR_RARITY = [
+  {
+    pet: REG_NAME_PET_ROUGH_TIERS_1,
+    percent: PERCENT_PET_ROUGH_TIERS_1,
+    supply: SUPPLY_PET_ROUGH_TIERS_1,
+  },
+  {
+    pet: REG_NAME_PET_ROUGH_TIERS_2,
+    percent: PERCENT_PET_ROUGH_TIERS_2,
+    supply: SUPPLY_PET_ROUGH_TIERS_2,
+  },
+  {
+    pet: REG_NAME_PET_ROUGH_TIERS_3,
+    percent: PERCENT_PET_ROUGH_TIERS_3,
+    supply: SUPPLY_PET_ROUGH_TIERS_3,
+  },
+  {
+    pet: REG_NAME_PET_CYBER_TIERS_1,
+    percent: PERCENT_PET_CYBER_TIERS_1,
+    supply: SUPPLY_PET_CYBER_TIERS_1,
+  },
+  {
+    pet: REG_NAME_PET_CYBER_TIERS_2,
+    percent: PERCENT_PET_CYBER_TIERS_2,
+    supply: SUPPLY_PET_CYBER_TIERS_2,
+  },
+  {
+    pet: REG_NAME_PET_CYBER_TIERS_3,
+    percent: PERCENT_PET_CYBER_TIERS_3,
+    supply: SUPPLY_PET_CYBER_TIERS_3,
+  },
+  {
+    pet: REG_NAME_PET_MATRIX_TIERS_1,
+    percent: PERCENT_PET_MATRIX_TIERS_1,
+    supply: SUPPLY_PET_MATRIX_TIERS_1,
+  },
+  {
+    pet: REG_NAME_PET_MATRIX_TIERS_2,
+    percent: PERCENT_PET_MATRIX_TIERS_2,
+    supply: SUPPLY_PET_MATRIX_TIERS_2,
+  },
+  {
+    pet: REG_NAME_PET_MATRIX_TIERS_3,
+    percent: PERCENT_PET_MATRIX_TIERS_3,
+    supply: SUPPLY_PET_MATRIX_TIERS_3,
+  },
+  {
+    pet: REG_NAME_PET_GOLDBOI_TIERS_1,
+    percent: PERCENT_PET_GOLDBOI_TIERS_1,
+    supply: SUPPLY_PET_GOLDBOI_TIERS_1,
+  },
+  {
+    pet: REG_NAME_PET_GOLDBOI_TIERS_2,
+    percent: PERCENT_PET_GOLDBOI_TIERS_2,
+    supply: SUPPLY_PET_GOLDBOI_TIERS_2,
+  },
+  {
+    pet: REG_NAME_PET_GOLDBOI_TIERS_3,
+    percent: PERCENT_PET_GOLDBOI_TIERS_3,
+    supply: SUPPLY_PET_GOLDBOI_TIERS_3,
+  },
+  {
+    pet: REG_NAME_PET_ROBOTER_TIERS_1,
+    percent: PERCENT_PET_ROBOTER_TIERS_1,
+    supply: SUPPLY_PET_ROBOTER_TIERS_1,
+  },
+  {
+    pet: REG_NAME_PET_ROBOTER_TIERS_2,
+    percent: PERCENT_PET_ROBOTER_TIERS_2,
+    supply: SUPPLY_PET_ROBOTER_TIERS_2,
+  },
+  {
+    pet: REG_NAME_PET_ROBOTER_TIERS_3,
+    percent: PERCENT_PET_ROBOTER_TIERS_3,
+    supply: SUPPLY_PET_ROBOTER_TIERS_3,
+  },
+  {
+    pet: REG_NAME_PET_BURNER_TIERS_1,
+    percent: PERCENT_PET_BURNER_TIERS_1,
+    supply: SUPPLY_PET_BURNER_TIERS_1,
+  },
+  {
+    pet: REG_NAME_PET_BURNER_TIERS_2,
+    percent: PERCENT_PET_BURNER_TIERS_2,
+    supply: SUPPLY_PET_BURNER_TIERS_2,
+  },
+  {
+    pet: REG_NAME_PET_BURNER_TIERS_3,
+    percent: PERCENT_PET_BURNER_TIERS_3,
+    supply: SUPPLY_PET_BURNER_TIERS_3,
+  },
+  {
+    pet: REG_NAME_PET_CELESTIAL_TIERS_1,
+    percent: PERCENT_PET_CELESTIAL_TIERS_1,
+    supply: SUPPLY_PET_CELESTIAL_TIERS_1,
+  },
+  {
+    pet: REG_NAME_PET_CELESTIAL_TIERS_2,
+    percent: PERCENT_PET_CELESTIAL_TIERS_2,
+    supply: SUPPLY_PET_CELESTIAL_TIERS_2,
+  },
+  {
+    pet: REG_NAME_PET_CELESTIAL_TIERS_3,
+    percent: PERCENT_PET_CELESTIAL_TIERS_3,
+    supply: SUPPLY_PET_CELESTIAL_TIERS_3,
+  },
+];
