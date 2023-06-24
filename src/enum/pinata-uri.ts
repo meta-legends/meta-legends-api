c2afbf4db499ecfe5fd80ba1cb33d3008f5dd0da
handosensei <handosensei@gmail.com>
2023-06-24 18:15:11 +0200

    CID IPFS metadata and content

---
+++ src/enum/pinata-uri.ts
@@ -0,0 +1,7 @@
+export const PINATA_URL = 'https://metalegends.mypinata.cloud/ipfs';
+export const URI_OG_PETS_METADATA =
+  '/Qmb1TTYKTyrw3LAVin46eNM2NaBbB3gzFDYDhXTpYNorPW';
+export const URI_OG_PETS_GIF =
+  '/QmSZpPMU5A9jfzifXBq28JdAq9Er9eoWFzaCGx4jvpiaKA';
+export const URI_OG_PETS_VIDEO =
+  '/QmRUxH2aS4Bm1cpSL7eVH732hJq46KmpG2HNuVzQMD4FHr';
