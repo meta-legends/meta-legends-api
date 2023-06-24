92020e2adb533c05f9bce4f489620271c88a6155
handosensei <handosensei@gmail.com>
2023-06-24 22:45:35 +0200

    Metadata OG Pets

---
+++ src/enum/metadata-attribute.ts
@@ -0,0 +1,299 @@
+import {
+  REG_NAME_PET_BURNER_TIERS_1,
+  REG_NAME_PET_BURNER_TIERS_2,
+  REG_NAME_PET_BURNER_TIERS_3,
+  REG_NAME_PET_CELESTIAL_TIERS_1,
+  REG_NAME_PET_CELESTIAL_TIERS_2,
+  REG_NAME_PET_CELESTIAL_TIERS_3,
+  REG_NAME_PET_CYBER_TIERS_1,
+  REG_NAME_PET_CYBER_TIERS_2,
+  REG_NAME_PET_CYBER_TIERS_3,
+  REG_NAME_PET_GOLDBOI_TIERS_1,
+  REG_NAME_PET_GOLDBOI_TIERS_2,
+  REG_NAME_PET_GOLDBOI_TIERS_3,
+  REG_NAME_PET_MATRIX_TIERS_1,
+  REG_NAME_PET_MATRIX_TIERS_2,
+  REG_NAME_PET_MATRIX_TIERS_3,
+  REG_NAME_PET_ROBOTER_TIERS_1,
+  REG_NAME_PET_ROBOTER_TIERS_2,
+  REG_NAME_PET_ROBOTER_TIERS_3,
+  REG_NAME_PET_ROUGH_TIERS_1,
+  REG_NAME_PET_ROUGH_TIERS_2,
+  REG_NAME_PET_ROUGH_TIERS_3,
+  SPECIFIC_NAME_PET_COUNCIL,
+  SPECIFIC_NAME_PET_GUARDIAN,
+  SPECIFIC_NAME_PET_HONORARY,
+  SPECIFIC_NAME_PET_JUDGE,
+  SPECIFIC_NAME_PET_TOP10,
+  SPECIFIC_PET_FOR_COUNCIL,
+  SPECIFIC_PET_FOR_GUARDIAN,
+  SPECIFIC_PET_FOR_HONORARY,
+  SPECIFIC_PET_FOR_JUDGE,
+  SPECIFIC_PET_FOR_TOP10,
+} from '@src/enum/og-pet-draw';
+
+export const OG_PET_SPECIFIC_METADATA = {
+  [SPECIFIC_PET_FOR_GUARDIAN]: [
+    {
+      trait_type: 'Class',
+      value: 'Roboter',
+    },
+    {
+      trait_type: 'Name',
+      value: SPECIFIC_NAME_PET_GUARDIAN,
+    },
+  ],
+  [SPECIFIC_PET_FOR_COUNCIL]: [
+    {
+      trait_type: 'Class',
+      value: 'Matrix Angel',
+    },
+    {
+      trait_type: 'Name',
+      value: SPECIFIC_NAME_PET_COUNCIL,
+    },
+  ],
+  [SPECIFIC_PET_FOR_TOP10]: [
+    {
+      trait_type: 'Class',
+      value: 'Celestial',
+    },
+    {
+      trait_type: 'Name',
+      value: SPECIFIC_NAME_PET_TOP10,
+    },
+  ],
+  [SPECIFIC_PET_FOR_JUDGE]: [
+    {
+      trait_type: 'Class',
+      value: 'Goldboi',
+    },
+    {
+      trait_type: 'Name',
+      value: SPECIFIC_NAME_PET_JUDGE,
+    },
+  ],
+  [SPECIFIC_PET_FOR_HONORARY]: [
+    {
+      trait_type: 'Class',
+      value: 'Burner',
+    },
+    {
+      trait_type: 'Name',
+      value: SPECIFIC_NAME_PET_HONORARY,
+    },
+  ],
+};
+
+export const OG_PET_RANDOM_METADATA = {
+  [REG_NAME_PET_ROUGH_TIERS_1]: [
+    {
+      trait_type: 'Class',
+      value: 'Rough',
+    },
+    {
+      trait_type: 'Name',
+      value: REG_NAME_PET_ROUGH_TIERS_1,
+    },
+  ],
+  [REG_NAME_PET_ROUGH_TIERS_2]: [
+    {
+      trait_type: 'Class',
+      value: 'Rough',
+    },
+    {
+      trait_type: 'Name',
+      value: REG_NAME_PET_ROUGH_TIERS_2,
+    },
+  ],
+  [REG_NAME_PET_ROUGH_TIERS_3]: [
+    {
+      trait_type: 'Class',
+      value: 'Rough',
+    },
+    {
+      trait_type: 'Name',
+      value: REG_NAME_PET_ROUGH_TIERS_3,
+    },
+  ],
+  [REG_NAME_PET_CYBER_TIERS_1]: [
+    {
+      trait_type: 'Class',
+      value: 'Cyber',
+    },
+    {
+      trait_type: 'Name',
+      value: REG_NAME_PET_CYBER_TIERS_1,
+    },
+  ],
+  [REG_NAME_PET_CYBER_TIERS_2]: [
+    {
+      trait_type: 'Class',
+      value: 'Cyber',
+    },
+    {
+      trait_type: 'Name',
+      value: REG_NAME_PET_CYBER_TIERS_2,
+    },
+  ],
+  [REG_NAME_PET_CYBER_TIERS_3]: [
+    {
+      trait_type: 'Class',
+      value: 'Cyber',
+    },
+    {
+      trait_type: 'Name',
+      value: REG_NAME_PET_CYBER_TIERS_3,
+    },
+  ],
+  [REG_NAME_PET_MATRIX_TIERS_1]: [
+    {
+      trait_type: 'Class',
+      value: 'Matrix',
+    },
+    {
+      trait_type: 'Name',
+      value: REG_NAME_PET_MATRIX_TIERS_1,
+    },
+  ],
+  [REG_NAME_PET_MATRIX_TIERS_2]: [
+    {
+      trait_type: 'Class',
+      value: 'Matrix',
+    },
+    {
+      trait_type: 'Name',
+      value: REG_NAME_PET_MATRIX_TIERS_2,
+    },
+  ],
+  [REG_NAME_PET_MATRIX_TIERS_3]: [
+    {
+      trait_type: 'Class',
+      value: 'Matrix',
+    },
+    {
+      trait_type: 'Name',
+      value: REG_NAME_PET_MATRIX_TIERS_3,
+    },
+  ],
+  [REG_NAME_PET_GOLDBOI_TIERS_1]: [
+    {
+      trait_type: 'Class',
+      value: 'Goldboi',
+    },
+    {
+      trait_type: 'Name',
+      value: REG_NAME_PET_GOLDBOI_TIERS_1,
+    },
+  ],
+  [REG_NAME_PET_GOLDBOI_TIERS_2]: [
+    {
+      trait_type: 'Class',
+      value: 'Goldboi',
+    },
+    {
+      trait_type: 'Name',
+      value: REG_NAME_PET_GOLDBOI_TIERS_2,
+    },
+  ],
+  [REG_NAME_PET_GOLDBOI_TIERS_3]: [
+    {
+      trait_type: 'Class',
+      value: 'Goldboi',
+    },
+    {
+      trait_type: 'Name',
+      value: REG_NAME_PET_GOLDBOI_TIERS_3,
+    },
+  ],
+  [REG_NAME_PET_ROBOTER_TIERS_1]: [
+    {
+      trait_type: 'Class',
+      value: 'Roboter',
+    },
+    {
+      trait_type: 'Name',
+      value: REG_NAME_PET_ROBOTER_TIERS_1,
+    },
+  ],
+  [REG_NAME_PET_ROBOTER_TIERS_2]: [
+    {
+      trait_type: 'Class',
+      value: 'Roboter',
+    },
+    {
+      trait_type: 'Name',
+      value: REG_NAME_PET_ROBOTER_TIERS_2,
+    },
+  ],
+  [REG_NAME_PET_ROBOTER_TIERS_3]: [
+    {
+      trait_type: 'Class',
+      value: 'Roboter',
+    },
+    {
+      trait_type: 'Name',
+      value: REG_NAME_PET_ROBOTER_TIERS_3,
+    },
+  ],
+  [REG_NAME_PET_BURNER_TIERS_1]: [
+    {
+      trait_type: 'Class',
+      value: 'Burner',
+    },
+    {
+      trait_type: 'Name',
+      value: REG_NAME_PET_BURNER_TIERS_1,
+    },
+  ],
+  [REG_NAME_PET_BURNER_TIERS_2]: [
+    {
+      trait_type: 'Class',
+      value: 'Burner',
+    },
+    {
+      trait_type: 'Name',
+      value: REG_NAME_PET_BURNER_TIERS_2,
+    },
+  ],
+  [REG_NAME_PET_BURNER_TIERS_3]: [
+    {
+      trait_type: 'Class',
+      value: 'Burner',
+    },
+    {
+      trait_type: 'Name',
+      value: REG_NAME_PET_BURNER_TIERS_3,
+    },
+  ],
+  [REG_NAME_PET_CELESTIAL_TIERS_1]: [
+    {
+      trait_type: 'Class',
+      value: 'Celestial',
+    },
+    {
+      trait_type: 'Name',
+      value: REG_NAME_PET_CELESTIAL_TIERS_1,
+    },
+  ],
+  [REG_NAME_PET_CELESTIAL_TIERS_2]: [
+    {
+      trait_type: 'Class',
+      value: 'Celestial',
+    },
+    {
+      trait_type: 'Name',
+      value: REG_NAME_PET_CELESTIAL_TIERS_2,
+    },
+  ],
+  [REG_NAME_PET_CELESTIAL_TIERS_3]: [
+    {
+      trait_type: 'Class',
+      value: 'Celestial',
+    },
+    {
+      trait_type: 'Name',
+      value: REG_NAME_PET_CELESTIAL_TIERS_3,
+    },
+  ],
+};
