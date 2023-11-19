b4139f003e3129b09d687b3c1148155abaceba28
handosensei <handosensei@gmail.com>
2023-11-19 22:25:51 +0100

    rename vehicles

--- src/enum/vehicle-draw.ts
+++ src/enum/vehicle-draw.ts
@@ -1,57 +1,57 @@
 import { PINATA_URL } from '@src/enum/pinata-uri';
 
-export const VEHICLE_CELESTIAL_REG_NAME = 'Celestial Speedster';
+export const VEHICLE_CELESTIAL_REG_NAME = 'Speedster';
 export const VEHICLE_CELESTIAL_CODE = 'celestial-speedster';
 export const VEHICLE_CELESTIAL_PERCENT = 7;
 export const VEHICLE_CELESTIAL_SUPPLY = 57;
 
-export const VEHICLE_BURNER_REG_NAME = 'Burner Speed Tank';
+export const VEHICLE_BURNER_REG_NAME = 'Speed Tank';
 export const VEHICLE_BURNER_CODE = 'burner-speed-tank';
 export const VEHICLE_BURNER_PERCENT = 9;
 export const VEHICLE_BURNER_SUPPLY = 74;
 
-export const VEHICLE_ROBOTER_REG_NAME = 'Roboter HoverTank';
+export const VEHICLE_ROBOTER_REG_NAME = 'HoverTank';
 export const VEHICLE_ROBOTER_CODE = 'roboter-hovertank';
 export const VEHICLE_ROBOTER_PERCENT = 11;
 export const VEHICLE_ROBOTER_SUPPLY = 90;
 
-export const VEHICLE_GOLDBOI_REG_NAME = 'Goldboi Tank';
+export const VEHICLE_GOLDBOI_REG_NAME = 'Tank';
 export const VEHICLE_GOLDBOI_CODE = 'goldboi-tank';
 export const VEHICLE_GOLDBOI_PERCENT = 13;
 export const VEHICLE_GOLDBOI_SUPPLY = 107;
 
-export const VEHICLE_MATRIX_REG_NAME = 'Matrix Racing Car';
+export const VEHICLE_MATRIX_REG_NAME = 'Racing Car';
 export const VEHICLE_MATRIX_CODE = 'matrix-racing-car';
 export const VEHICLE_MATRIX_PERCENT = 17;
 export const VEHICLE_MATRIX_SUPPLY = 140;
 
-export const VEHICLE_CYBER_REG_NAME = 'Cyber Bike';
+export const VEHICLE_CYBER_REG_NAME = 'Bike';
 export const VEHICLE_CYBER_CODE = 'cyber-bike';
 export const VEHICLE_CYBER_PERCENT = 20;
 export const VEHICLE_CYBER_SUPPLY = 164;
 
-export const VEHICLE_ROUGH_REG_NAME = 'Rough Buggy';
+export const VEHICLE_ROUGH_REG_NAME = 'Buggy';
 export const VEHICLE_ROUGH_CODE = 'rough-buggy';
 export const VEHICLE_ROUGH_PERCENT = 23;
 export const VEHICLE_ROUGH_SUPPLY = 189;
 
-export const VEHICLE_COUNCIL_REG_NAME = 'Roboter HoverTank Council';
+export const VEHICLE_COUNCIL_REG_NAME = 'HoverTank Council';
 export const VEHICLE_COUNCIL_CODE = 'roboter-hovertank-council';
 export const VEHICLE_COUNCIL_SUPPLY = 250;
 
-export const VEHICLE_HONORARY_REG_NAME = 'Burner Speed Tank Honorary';
+export const VEHICLE_HONORARY_REG_NAME = 'Speed Tank Honorary';
 export const VEHICLE_HONORARY_CODE = 'burner-speed-tank-honorary';
 export const VEHICLE_HONORARY_SUPPLY = 66;
 
-export const VEHICLE_WHALE_REG_NAME = 'Celestial Speedster Whale';
+export const VEHICLE_WHALE_REG_NAME = 'Speedster Whale';
 export const VEHICLE_WHALE_CODE = 'celestial-speedster-whale';
 export const VEHICLE_WHALE_SUPPLY = 31;
 
-export const VEHICLE_GUARDIAN_REG_NAME = 'Goldboi Tank Guardian';
+export const VEHICLE_GUARDIAN_REG_NAME = 'Tank Guardian';
 export const VEHICLE_GUARDIAN_CODE = 'goldboi-tank-guardian';
 export const VEHICLE_GUARDIAN_SUPPLY = 22;
 
-export const VEHICLE_JUDGE_REG_NAME = 'Cyber Bike Judge';
+export const VEHICLE_JUDGE_REG_NAME = 'Bike Judge';
 export const VEHICLE_JUDGE_CODE = 'cyber-bike-judge';
 export const VEHICLE_JUDGE_SUPPLY = 10;
 
