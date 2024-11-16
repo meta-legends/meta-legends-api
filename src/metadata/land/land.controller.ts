b2e7ee9ecfdf0a599bf21eb8d2cd85a16d7cb009
handosensei <handosensei@gmail.com>
2024-11-16 16:11:52 +0100

    Use cache to land image and metadata

--- src/metadata/land/land.controller.ts
+++ src/metadata/land/land.controller.ts
@@ -11,7 +11,7 @@ import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { Cache } from 'cache-manager';
 import { LandWishService } from '@src/eligibility/land-wish/land-wish.service';
 import { Public } from '@src/common/decorators/public.decorator';
-import {createReadStream} from "fs";
+import { createReadStream } from 'fs';
 import { join } from 'path';
 
 @Controller('lands')
@@ -25,13 +25,17 @@ export class LandController {
   @Header('content-type', 'application/json')
   @Get('/metadata/:id')
   async getMetadata(@Param('id') id: number) {
+    const cache = await this.cacheManager.get('land-minted-metadata-get-' + id);
+    if (cache != null) {
+      return cache;
+    }
     const landWish = await this.landWishService.get(id);
     if (landWish === null) {
       throw new NotFoundException('Unknow token ID ' + id);
     }
     const className = landWish.land.class;
     const category = landWish.category;
-    return {
+    const result = {
       name: 'Meta-Life OG Land #' + id,
       description:
         'This NFT represents an OG Land whose abilities will be at their full potential in Meta Life, the metaverse by Meta Legends',
@@ -53,18 +57,27 @@ export class LandController {
         },
       ],
     };
+    await this.cacheManager.set(
+      'land-minted-metadata-get-' + id,
+      result,
+      86400000,
+    );
+    return result;
   }
 
   @Public()
   @Get('/:id/image')
-  async getImage(@Param('id') id: number): Promise<StreamableFile> {
+  async getImage(@Param('id') id: number): Promise<unknown | StreamableFile> {
+    const cache = await this.cacheManager.get('land-minted-image-get-' + id);
+    if (cache != null) {
+      return cache;
+    }
     const landWish = await this.landWishService.get(id);
     if (landWish === null) {
       throw new NotFoundException('Unknow token ID ' + id);
     }
 
     const land = landWish.land;
-    console.log(land);
     const file = createReadStream(
       join(
         __dirname,
@@ -73,9 +86,15 @@ export class LandController {
         }.png`,
       ),
     );
-    return new StreamableFile(file, {
+    const result = new StreamableFile(file, {
       type: 'image/png',
       disposition: 'attachment; filename="land.json"',
     });
+    await this.cacheManager.set(
+      'land-minted-image-get-' + id,
+      result,
+      86400000,
+    );
+    return result;
   }
 }
